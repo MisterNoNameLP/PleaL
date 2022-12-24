2d4d49df369a16e8fc02e09dd342d8952cc960b1
MisterNoNameLP <misternonamelp@web.de>
2022-12-24 14:15:22 +0100

    Misc

--- src/executionScript.lua
+++ src/executionScript.lua
@@ -2,7 +2,7 @@
     This script makes it possible to easly axecute PleaL script from the command line.
     It is supposed to be shippen within an all-in-one script wich is createt automatically. But it can be executet in its pure form as well.
     For more information see the official PleaL readme.
-    
+
     https://github.com/MisterNoNameLP/PleaL#building
 ]]
 
@@ -11,6 +11,8 @@ package.path = package.path .. ";src/?.lua;src/thirdParty/?.lua"
 local ut = require("UT")
 local pleal = require("plealTranspilerAPI")
 local argparse = require("argparse")
+
+
 
 print("This script is not finished yet.")
 os.exit(0)
